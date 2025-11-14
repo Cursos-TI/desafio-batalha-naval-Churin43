a929a4f798552e52ea9eaa8f7d821249d4cff81e
th3byss <101571320+Churin43@users.noreply.github.com>
2025-11-14 10:31:34 -0300

    Commit nivel mestre desafio Batalha naval
    Commit nivel mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
-#define TAM 10      // tamanho do tabuleiro 10x10
-#define NAVIO 3     // tamanho dos navios (3 posições)
-#define AGUA 0      //  água
-#define NAV 3       // navio
+#define TAM 10
+#define NAVIO 3
+#define AGUA 0
+#define NAV 3
+#define HAB 5
 
 int posicao_livre(int tab[TAM][TAM], int coords[3][2]) {
     for (int i = 0; i < NAVIO; i++) {
         int lin = coords[i][0];
         int col = coords[i][1];
-
-        if (tab[lin][col] == NAV) {
-            return 0; // posição ocupada -> não pode
-        }
+        if (tab[lin][col] == NAV) return 0;
     }
-    return 1; // todas livres
+    return 1;
 }
-
 
 void posicionar(int tab[TAM][TAM], int coords[3][2]) {
     for (int i = 0; i < NAVIO; i++) {
@@ -26,65 +23,101 @@ void posicionar(int tab[TAM][TAM], int coords[3][2]) {
     }
 }
 
-int main() {
+void aplicar_habilidade(int tab[TAM][TAM], int hab[5][5], int centroL, int centroC) {
 
-    int tabuleiro[TAM][TAM];
+    int offset = 5 / 2;
 
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
 
-    for (int i = 0; i < TAM; i++) {
-        for (int j = 0; j < TAM; j++) {
-            tabuleiro[i][j] = AGUA;
+            if (hab[i][j] == 1) {
+                int lin = centroL + (i - offset);
+                int col = centroC + (j - offset);
+
+                if (lin >= 0 && lin < TAM && col >= 0 && col < TAM) {
+                    if (tab[lin][col] != NAV)
+                        tab[lin][col] = HAB;
+                }
+            }
         }
     }
+}
 
+int main() {
 
-    int navio1[3][2] = {
-        {2, 1}, {2, 2}, {2, 3}
-    };
+    int tab[TAM][TAM];
 
-    int navio2[3][2] = {
-        {5, 4}, {6, 4}, {7, 4}
-    };
+    for (int i = 0; i < TAM; i++)
+        for (int j = 0; j < TAM; j++)
+            tab[i][j] = AGUA;
 
-    int navio3[3][2] = {
-        {1, 1}, {2, 2}, {3, 3}
-    };
-
-
-    int navio4[3][2] = {
-        {8, 1}, {7, 2}, {6, 3}
-    };
-
+    int navio1[3][2] = { {2,1}, {2,2}, {2,3} };
+    int navio2[3][2] = { {5,4}, {6,4}, {7,4} };
+    int navio3[3][2] = { {1,1}, {2,2}, {3,3} };
+    int navio4[3][2] = { {8,1}, {7,2}, {6,3} };
 
     int *navios[4] = { (int*)navio1, (int*)navio2, (int*)navio3, (int*)navio4 };
 
     for (int n = 0; n < 4; n++) {
         int (*nav)[2] = (int (*)[2])navios[n];
 
-        for (int i = 0; i < NAVIO; i++) {
-            int lin = nav[i][0];
-            int col = nav[i][1];
-
-            if (lin < 0 || lin >= TAM || col < 0 || col >= TAM) {
-                printf("Erro: coordenadas invalidas no navio %d\n", n+1);
-                return 1;
-            }
-        }
-
-        if (!posicao_livre(tabuleiro, nav)) {
-            printf("Erro: sobreposicao detectada no navio %d\n", n+1);
+        if (!posicao_livre(tab, nav)) {
+            printf("Erro: sobreposição de navios!\n");
             return 1;
         }
 
-        posicionar(tabuleiro, nav);
+        posicionar(tab, nav);
     }
 
+    int cone[5][5];
+    int cruz[5][5];
+    int octaedro[5][5];
 
-    printf("\nTABULEIRO FINAL\n");
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (j >= 2 - i && j <= 2 + i)
+                cone[i][j] = 1;
+            else
+                cone[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 2 || j == 2)
+                cruz[i][j] = 1;
+            else
+                cruz[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            int dist = abs(i - 2) + abs(j - 2);
+
+            if (dist <= 2)
+                octaedro[i][j] = 1;
+            else
+                octaedro[i][j] = 0;
+        }
+    }
+
+    int centroConeL = 4, centroConeC = 4;
+    int centroCruzL = 7, centroCruzC = 7;
+    int centroOctL  = 1, centroOctC  = 8;
+
+    aplicar_habilidade(tab, cone, centroConeL, centroConeC);
+    aplicar_habilidade(tab, cruz, centroCruzL, centroCruzC);
+    aplicar_habilidade(tab, octaedro, centroOctL, centroOctC);
+
+    printf("\n TABULEIRO FINAL \n");
 
     for (int i = 0; i < TAM; i++) {
         for (int j = 0; j < TAM; j++) {
-            printf("%d ", tabuleiro[i][j]);
+
+            if (tab[i][j] == AGUA) printf("0 ");
+            else if (tab[i][j] == NAV) printf("3 ");
+            else if (tab[i][j] == HAB) printf("5 ");
         }
         printf("\n");
     }
